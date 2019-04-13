71a849434925c0357ee7a591c969624de420b0b8
Oskar Michalak <o.michalak@actumlab.com>
2019-04-13 18:53:39 +0200

    Add AlarmFragment

--- AndroidApp/app/src/main/java/com/mefki/mainactivity/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/mefki/mainactivity/MainActivity.kt
@@ -3,22 +3,22 @@ package com.mefki.mainactivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.mefki.mainactivity.alarms.AlarmsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_find_now -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_alarms -> {
+                showFragment(AlarmsFragment())
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_settings -> {
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +30,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun showFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content_main, fragment)
+            .commit()
     }
 }
