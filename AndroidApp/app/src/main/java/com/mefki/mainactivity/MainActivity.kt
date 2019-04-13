d3fd1f4abf68619581bf16c5c629a28c3b343ec3
Szymon <szym.trzebiatowski@gmail.com>
2019-04-13 22:36:48 +0200

    add MapsActivity

--- AndroidApp/app/src/main/java/com/mefki/mainactivity/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/mefki/mainactivity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.mefki.mainactivity
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import com.mefki.mainactivity.maps.MapsActivity
 import androidx.fragment.app.Fragment
 import com.mefki.mainactivity.alarms.AlarmsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class
+MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -28,6 +31,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
